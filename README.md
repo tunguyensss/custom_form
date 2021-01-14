efac7658a57acc3881e6a105e7ea1c905d1ea151
tu.nguyen <tu.nguyen@successsoftware.global>
2021-01-14 11:10:23 +0700

    custom form

---
+++ README.md
@@ -0,0 +1,7 @@
+# CUSTOM FORM
+
+## HOW TO USE
+
+run `npm install` to install bootstrap
+
+run `yarn build-scss` or `npm run build-script` to build css from main.scss
---
+++ assets/js/validate-form.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('.needs-validation');
+
+form.addEventListener('submit', function (event) {
+    if(form.checkValidity() === false){
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    form.classList.add('was-validated');
+})
